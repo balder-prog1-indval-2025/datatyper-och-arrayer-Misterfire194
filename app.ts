af77fa1702fa7a63bf996702bfd2ab6b6a12669a
Elliot <ebu0729@student.skelleftea.se>
2025-10-10 09:43:01 +0200

    Påbörjat med Datatyper och arrayer. Ej klar

---
+++ app.ts
@@ -0,0 +1,18 @@
+/*
+write("Deluppgift 1")
+let x = await read("Förnamn?: ")
+let y = await read("Efternamn?: ")
+write ("Dina initialer är " +x[0]+y[0])
+
+
+write ("Deluppgift 2 ")
+let x = await read("Förnamn?: ")
+let y = await read("Efternamn?: ")
+write ("Användarnamn är: " +x.slice(0,3)+y.slice(0,3))
+
+*/
+
+
+
+
+write("Deluppgift 3: ")
