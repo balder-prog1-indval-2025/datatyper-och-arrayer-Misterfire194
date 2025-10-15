0d5e5b4e66c67862342e2ecdc14a0ca429aab709
Elliot <ebu0729@student.skelleftea.se>
2025-10-15 09:45:51 +0200

    Fortsatt arbete

--- app.ts
+++ app.ts
@@ -8,11 +8,40 @@ write ("Dina initialer är " +x[0]+y[0])
 write ("Deluppgift 2 ")
 let x = await read("Förnamn?: ")
 let y = await read("Efternamn?: ")
-write ("Användarnamn är: " +x.slice(0,3)+y.slice(0,3))
 
-*/
+write ("Användarnamn är: " +x.slice(0,3).toLowerCase()+y.slice(0,3).toLowerCase())
+
+
 
 
 
 
 write("Deluppgift 3: ")
+let x = await read ("Telefonnummer (bara siffror)? ")
+ if (x.length == 10) {
+    write ("Telefonnummer med bindestreck = " + x.replace(x[4],"-"+ x[4]))
+
+    write ("Telefonnummer utan bindestreck = " + x.replace(x[4]," "+ x[4]))
+ }
+else {
+    write("Inget telefonnumer!")
+}
+
+
+
+write ("Deluppgift 4:")
+let x = await read("Telefonnummer (med bindestreck)? ")
+if (x.includes("-") && x.length == 10) {
+
+let y = x.indexOf("-")
+write ("Riktnummer = " + x.slice(0, y))
+write ("Abonnentnummer = " + x.slice(y+1,10))
+} else {
+    write ("Inget telefonnummer!")
+}
+*/
+
+write ("Deluppgift 5: ")
+let x = +await read ("Minuter: ")
+let y= x/60
+write ("Timmar = " + y[1])
