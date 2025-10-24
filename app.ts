9997eec53d055adf817691ccd97ddcfb4987d42c
Elliot <ebu0729@student.skelleftea.se>
2025-10-24 09:45:08 +0200

    Klar med normala uppgifterna, extra uppgifter kvar

--- app.ts
+++ app.ts
@@ -54,13 +54,75 @@ write("Deluppgift 6: ")
 let x= +await read ("Radie: ")
 let area = x**2 * Math.PI
 let omkrets = (2*x) * Math.PI
-write("Area = " + area)
-write("Omkrets = " + omkrets)
-*/
+write("Area = " + Math.round(area))
+write("Omkrets = " + Math.round(omkrets))
+
 
 write ("Deluppgift 7: ")
 let a = +await read ("Första sidlängden: ")
 let b = +await read ("Andra sidlängden: ")
 let c = +await read ("Sista sidlängden: ")
-let x = (a**) + b**   
+let x = Math.sqrt(a**2 + b**2 + c**2)    
 write ("Längsta diagonal = " + x)
+
+
+write ("Deluppgift 8:")
+let a = [2,3,5,7,11,13,17,19]
+write (a)
+write (a[0])
+write (a[7])
+write (a.length)
+a.push(23)
+write (a)
+write (a.length)
+
+
+write ("Deluppgift 9:")
+let städer = ["Rom", "Paris", "Berlin", "Moskva"]
+let länder = ["Italien", "Frankrike", "Tyskland", "Ryssland"]
+
+städer.push ("Stockholm")
+länder.push ("Sverige")
+for (let i = 0; i < städer.length; i++) {
+    write (städer[i] + " är huvudstaden till " + länder[i])
+} 
+
+
+write ("Deluppgift 10:")
+let x = +await read ("Tal 1: ")
+let y = +await read ("Tal 2: ")
+let z = +await read ("Tal 3: ")
+let v = +await read ("Tal 4: ")
+let g = +await read ("Tal 5: ")
+
+let a = [g,v,z,y,x] 
+
+for(let i = 0; i < a.length; i++) {
+    write(a[i])
+}
+
+
+write ("Deluppgift 11:")
+let a = []
+let x = +await read("Antal: ")
+
+for (let i = 0; i < x; i++) {
+   let Tal = +await read("Tal " + (i+1) + ":" )
+   a.push(Tal)
+}
+
+for (let i = (a.length-1); i>=0; i-=1) {
+    write (a[i])
+}
+*/
+
+write("Extrauppgift 1:")
+let x = await read("Telefonnummer (med bindestreck)? ")
+if (x.includes("-") && x.length == 10 && x.includes ("1" || "2" || "3" || "4" || "5" || "6" || "7" || "8" || "9" || "0") && x.slice(4,5) == "-") {
+
+let y = x.indexOf("-")
+write ("Riktnummer = " + x.slice(0, y))
+write ("Abonnentnummer = " + x.slice(y+1,10))
+} else {
+    write ("Inget telefonnummer!")
+}
